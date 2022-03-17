2eabc1681da99488c995e5840eed6e7a4fb02ca9
WiccaCrow <selletrer@mail.ru>
2022-03-17 15:54:30 +0300

    enable_if finished

--- project/include/ft_enable_if.hpp
+++ project/include/ft_enable_if.hpp
@@ -9,6 +9,9 @@ struct enable_if {};
 template<class T>
 struct enable_if<true, T> { typedef T type; };
 
+template< bool B, class T = void >
+using enable_if_t = typename enable_if<B,T>::type;
+
 };
 
 #endif
--- project/src/test_enable_if.cpp
+++ project/src/test_enable_if.cpp
@@ -3,17 +3,24 @@
 #include <ft_enable_if.hpp>
 #include <type_traits>
 
-template <class T>
-void foo1(const T &) { std::cout << "foo1" << std::endl; }
+template <typename T>
+std::string str(T t,  ft::enable_if_t<std::is_same_v<T, std::string> >* = nullptr ) {
+    std::cout << "is string: ";
+  return t;
+}
 
-template < class T, class = typename std::enable_if<std::is_integral<T>::value, T>::type>
-void foo2(const T &) { std::cout << "foo2" << std::endl; }
-
-struct X {};
+template <typename T>
+std::string str(T t, ft::enable_if_t<!(std::is_same_v<T, std::string>)>* = nullptr) {
+    std::cout << "not string: ";
+  return std::to_string(t);
+}
 
 int main()
 {
-    // foo2(X());
-    foo1(5);
+
+    std::string c = "hello";
+    std::cout << str(5) << std::endl;
+    std::cout << str(c) << std::endl;
+    std::cout << str(5u) << std::endl;
     return 0;
 }
